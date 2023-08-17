39820b783e80e57072847262955b228ea9815a6a
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-08-17 08:45:18 +0000

    add express upp

--- scraper.js
+++ scraper.js
@@ -4,19 +4,9 @@ const axios = require("axios");
 async function run() {
   const response = await axios("https://dev.to/");
   const $ = cheerio.load(response.data);
-  // console.log($(".substories").find("a").text());
   $(".crayons-story__title a").each(function (i, elem) {
-    //console.log("text", i, $(this).find(".crayons-story__title a").text());
     console.log("text", i, $(this).text());
     console.log("text", i, $(this).attr("href"));
-    /*console.log(
-      "href",
-      i,
-      $(this).find(".crayons-story__title a").attr("href"),
-    );*/
   });
 }
 run();
-
-//crayons-story :
-//cr$('h2.title').find('.subtitle').text();////a
