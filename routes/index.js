e6d4cf29c6f2d1f6ffb1a05b3d599c4563f44cad
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-08-17 09:03:17 +0000

    success recoup

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require("express");
 var router = express.Router();
+const cheerio = require("cheerio");
+const axios = require("axios");
+const arr2 = [];
+async function run() {
+  const response = await axios("https://dev.to/");
+  const $ = cheerio.load(response.data);
+  $(".crayons-story__title a").each(function (i, elem) {
+    console.log("text", i, $(this).text());
+    console.log("text", i, $(this).attr("href"));
+    arr2.push({ title: $(this).text(), link: $(this).attr("href") });
+  });
+}
+run();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -14,7 +27,7 @@ router.get("/", function (req, res, next) {
     },
   ];
 
-  res.render("index", { title: "Express", arr });
+  res.render("index", { title: "Express", arr2 });
 });
 
 module.exports = router;
